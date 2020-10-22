3dcee9995c0cf60e4a44cfe3a83897eb1ad5c669
MartinFloresGrez <45276974+MartinFloresGrez@users.noreply.github.com>
2020-10-22 14:26:57 -0300

    horoscopo

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,6 +4,7 @@ require 'telegram/bot'
 require_relative 'beer.rb'
 require_relative 'covid.rb'
 require_relative 'paraderos.rb'
+require_relative 'horoscopo.rb'
 
 token = ENV['TELEGRAM_TOKEN']
 
@@ -43,6 +44,14 @@ Telegram::Bot::Client.run(token) do |bot|
         query = message.text.split(' ')[1..-1].join(' ')
         paradero = Paradero.new(query)
         data = paradero.execute
+        bot.api.send_message(
+          chat_id: message.chat.id,
+          text: data
+        )
+      when /^horoscopo\s+.*$/
+        query = message.text.split(' ')[1..-1].join(' ')
+        horoscopo = Horoscopo.new(query)
+        data = horoscopo.execute
         bot.api.send_message(
           chat_id: message.chat.id,
           text: data
